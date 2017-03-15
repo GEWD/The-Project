9ebe040861a690f1a510f3dc4e8580dd5557731d
Gary Wong <gary.wonghg@gmail.com>
2017-03-15 12:55:59 -0700

    Move db folder to server folder

---
+++ server/db/index.js
@@ -0,0 +1,16 @@
+// server/db/index.js uses the mysql npm module to connect to the database server running on your computer
+var Sequelize = require('sequelize');
+// without password and options
+let sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'mysql',
+  port: '3000' // CHECK: '3306' for mysql?
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Db connection has been established successfully.');
+  }, function (err) { 
+    console.log('Unable to connect to the database:', err);
+  });
+
