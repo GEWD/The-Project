fceb7bccb7bbcb435572ea91a5b2c65dc95b7a3e
Duy Nguyen <nguyenaiden@gmail.com>
2017-03-15 18:29:58 -0700

    Set up basic receipt Upload component

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CreateTrip from './components/CreateTrip.jsx';
+import UploadReceipt from './components/Upload.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.UploadReceipt = this.UploadReceipt.bind(this);
+  }
 
+  UploadReceipt(image) {
+    $.ajax({
+      type: 'POST',
+      url:'/upload',
+      data: {photo: image},
+      success: (data,results) => {
+        console.log('Image successfully uploaded');
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
   }
 
   render() {
@@ -17,6 +32,7 @@ class App extends React.Component {
         <div>
           <h3>CreateTrip test</h3>
           <CreateTrip />
+          <UploadReceipt upload={this.UploadReceipt.bind(this)}/>
         </div>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -9,6 +9,20 @@ app.get('/', function(req, res) {
   res.send('hello world')
 });
 
+app.post('/upload', function(req,res) {
+  var body = req.body;
+  console.log(body);
+  res.sendStatus(201);
+  //req.body should include receipt name, total, receipt_link;
+  //should be an insert query
+
+})
+
+app.post('/upload/delete', function(req,res) {
+  //req.body should include receipt name, total, receipt_link;
+  //should be a delete query
+})
+
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
