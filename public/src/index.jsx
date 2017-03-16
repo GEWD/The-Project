521ce6e2a2b3e1ed71c204b32e4ea31006c4e4c4
Duy Nguyen <nguyenaiden@gmail.com>
2017-03-15 22:11:20 -0700

    Added photo upload feature

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -6,24 +6,11 @@ import CreateTrip from './components/CreateTrip.jsx';
 import UploadReceipt from './components/Upload.jsx';
 import Login from './components/Login.jsx';
 
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.UploadReceipt = this.UploadReceipt.bind(this);
-  }
-
-  UploadReceipt(image) {
-    $.ajax({
-      type: 'POST',
-      url:'/upload',
-      data: {photo: image},
-      success: (data,results) => {
-        console.log('Image successfully uploaded');
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    })
   }
 
   render() {
@@ -38,7 +25,7 @@ class App extends React.Component {
             </ul>
             <Route exact path ="/" component={TripSummary}/>
             <Route path ="/login" component={Login}/>
-            <Route path ="/upload-receipt" component={UploadReceipt} upload={this.UploadReceipt.bind(this)}/>
+            <Route path ="/upload-receipt" component={UploadReceipt}/>
           </div>
         </Router>
       </div>
