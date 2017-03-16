aaacec359146bf429cbce008001e69ebf1fa9b2e
whitzhu <whitzhu@gmail.com>
2017-03-15 19:33:56 -0700

    work in progress - adding passport-facebook authentication to server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,81 @@ const cors = require('cors');
 const app = express();
 const model = require('./model');
 
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var KEYS = require('../env/KEYS.js');
+
 app.use( bodyParser.json() );
 app.use(cors());
 app.use(express.static(__dirname + '/../public/dist'));
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: KEYS.SESSION_SECRET, resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+    clientID: KEYS.FB_APP_CLIENTID,
+    clientSecret: KEYS.FB_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    User
+    console.log('profile', profile);
+  }
+));
+
+// Define routes.
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+// route for facebook authentication and login
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+// handle the callback after facebook has authenticated the user
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
+
+// // route middleware to make sure a user is logged in
+// function isLoggedIn(req, res, next) {
+
+//   // if user is authenticated in the session, carry on
+//   if (req.isAuthenticated())
+//       return next();
+
+//   // if they aren't redirect them to the home page
+//   res.redirect('/');
+// }
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 app.get('/', function(req, res) {
   res.send('hello world');
@@ -36,4 +108,3 @@ app.post('/upload/delete', function(req,res) {
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
-
