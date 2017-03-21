6f8950831ba9170d3451d5cd5a9bcd87d3803010
Eugene Song <eugenessong@gmail.com>
2017-03-20 18:14:13 -0700

    Add route handler to visionAPI in index.js and helper functions in api/vision.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ const path = require('path');
 
 var localStorage = {};
 
+
+const gVision = require('./api/vision.js');
+
+
 app.use( bodyParser.json() );
 app.use(cors());
 app.use(express.static(__dirname + '/../public/dist'));
@@ -45,22 +49,23 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 passport.use(new FacebookStrategy({
-    clientID: KEYS.facebookAuth.clientID,
-    clientSecret: KEYS.facebookAuth.clientSecret,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-    profileFields: ['id', 'email', 'displayName', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
-  },
+  clientID: KEYS.facebookAuth.clientID,
+  clientSecret: KEYS.facebookAuth.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'email', 'displayName', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
+},
+
   function(accessToken, refreshToken, profile, cb) {
     process.nextTick(function () {
-    let userInfo = {
-      name: profile._json.name,
-      fb_id: profile._json.id,
-      token: accessToken,
-      email: profile._json.email
-    };
-    db.createNewUser(userInfo);
-    return cb(null, userInfo);
-    })
+      let userInfo = {
+        name: profile._json.name,
+        fb_id: profile._json.id,
+        token: accessToken,
+        email: profile._json.email
+      };
+      db.createNewUser(userInfo);
+      return cb(null, userInfo);
+    });
   }
 ));
 
@@ -70,7 +75,7 @@ function checkAuthentication(req, res, next) {
     //if user is loged in, req.isAuthenticated() will return true
     next();
   } else {
-    res.redirect("/login");
+    res.redirect('/login');
   }
 }
 
@@ -90,7 +95,7 @@ app.get('/auth/facebook/callback',
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
-});
+  });
 
 // // test database functions
 // app.get('/', db.getAllUsers);
@@ -115,7 +120,7 @@ app.get('/logout', authHelper, function(req, res) {
 });
 
 app.get('/verify', authHelper, function(req, res) {
-  let isAuthenticated =  req.isAuthenticated() ? true : false;
+  let isAuthenticated = req.isAuthenticated() ? true : false;
   res.send(req.isAuthenticated());
 });
 
@@ -147,10 +152,10 @@ app.post('/testTripName', function(req, res) {
   res.redirect('/upload-receipt');
 });
 
-app.post('/upload', function(req,res) {
+app.post('/upload', function(req, res) {
   //req.body should include receipt name, total, receipt_link;
   //should be an insert query
-   if (!req.files) {
+  if (!req.files) {
     return res.status(400).send('No files were uploaded.');
   }
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
@@ -162,20 +167,37 @@ app.post('/upload', function(req,res) {
       return res.status(500).send(err);
     }
     cloudinary.uploader.upload(__dirname + '/temp/filename.jpg', function(results) {
-      var params = [1,1,1,'cat',results.url, 150,10,15];
-      db.addReceipt(params, function(err,data) {
+      var params = [1, 1, 1, 'cat', results.url, 150, 10, 15];
+      db.addReceipt(params, function(err, data) {
         console.log(data);
         res.send('File uploaded!');
-      })
-    })
+      });
+    });
   });
 
-})
+});
 
-app.post('/upload/delete', function(req,res) {
+app.post('/upload/delete', function(req, res) {
   //req.body should include receipt name, total, receipt_link;
   //should be a delete query
-})
+});
+
+app.post('/vision', function(req, res) {
+  let image = './api/test.jpg';
+  gVision.promisifiedDetectText(image)
+  .then(function(results) {
+    res.send(results);
+  })
+  .error(function(e) {
+    console.log('Error received in appPost, promisifiedDetectText:', e);
+  });
+});
+
+app.post('/test', function(req, res) {
+  // res.send(image);
+  res.send(__dirname);
+});
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
