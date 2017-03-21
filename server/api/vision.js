856e81ea619b57583268a80f9b2ceecaa8b69101
Eugene Song <eugenessong@gmail.com>
2017-03-20 18:14:13 -0700

    Add route handler to visionAPI in index.js and helper functions in api/vision.js

--- server/api/vision.js
+++ server/api/vision.js
@@ -1,12 +1,64 @@
-const gcloud = require('gcloud')({
-  keyFilename: './goog.json',
-  projectId: 'gewd-161701'
+const express = require('express');
+const gcloud = require('google-cloud');
+const app = express();
+const Promise = require('bluebird');
+
+//Google-cloud mod setup
+const vision = gcloud.vision;
+const visionClient = vision({
+  projectId: 'gewd-161701',
+  keyFilename: '../env/goog.json'
 });
 
-var vision = gcloud.vision();
+//Google-cloud usage
+// let image = './test.jpg';
 
-var image = 'image.jpg';
+// visionClient.detectText(image, function(err, text, apiResponse) {
+//   if (err) {
+//     console.log('Received error within vision:detectText', err);
+//   } else {
+//     console.log(`Text found in ${image}:`, text);
+//     console.log('Response from api:', apiResponse);
+//   }
+// });
 
-vision.detectText('image.jpg', function(err, text, apiResponse) {
-  // text = ['This was text found in the image']
-});
+exports.promisifiedDetectText = function(image) {
+  return new Promise(function(resolve, reject) {
+    visionClient.detectText(image, function(err, text) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(text);
+      }
+    });
+  });
+};
+
+// exports.promisifiedDetectText(image)
+// .then(function(results) {
+//   console.log(results);
+// })
+// .error(function(e) {
+//   console.log('Error received in promisifiedDetectText:', e);
+// });
+
+// exports.scanPic = function(image, function(err, results) {
+//   if (err) {
+//     console.log('Received err in scanPic:', err)
+//   } else {
+//     visionClient.detectText(image, function(err, text, apiResponse) {
+//       if (err) {
+//         console.log('Received error within vision:detectText', err);
+//       } else {
+//         console.log(`Text found in ${image}:`, text);
+//         console.log('Response from api:', apiResponse);
+//       }
+//     });
+//   }
+// })
+
+// exports.scanPic = function(image) {
+//   return new Promise(function(resolve, reject) {
+
+//   })
+// }
