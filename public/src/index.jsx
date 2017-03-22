a3a2974f24adb846bd097a9c00f5e8f8341690d2
Duy Nguyen <nguyenaiden@gmail.com>
2017-03-22 11:44:51 -0700

    Added Itemization Page

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -5,12 +5,38 @@ import TripSummary from './components/TripSummary.jsx';
 import CreateTrip from './components/CreateTrip.jsx';
 import UploadReceipt from './components/Upload.jsx';
 import Login from './components/Login.jsx';
-
+import CreateItem from './components/CreateItem.jsx';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items:[],
+      name:'',
+      amount: 0
+    };
+    this.addItem = this.addItem.bind(this);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onPriceChange = this.onPriceChange.bind(this);
+  }
+
+  addItem (itemArray){
+    this.setState({
+      items: this.state.items.concat([[this.state.name, this.state.amount]])
+    })
+  }
+
+  onNameChange(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  onPriceChange(event) {
+    this.setState({
+      amount: event.target.value
+    })
   }
 
   render() {
@@ -22,10 +48,12 @@ class App extends React.Component {
               <li><Link to="/">Home</Link></li>
               <li><Link to="/upload-receipt">Upload Receipt</Link></li>
               <li><Link to="/login">Login</Link></li>
+              {/*<li><Link to="/additem">Add Item</Link></li>*/}
             </ul>
             <Route exact path ="/" component={TripSummary}/>
             <Route path ="/login" component={Login}/>
             <Route path ="/upload-receipt" component={UploadReceipt}/>
+            <CreateItem addItem={this.addItem} itemName={this.state.name} itemAmount={this.state.amount} items={this.state.items} onNameChange= {this.onNameChange} onPriceChange ={this.onPriceChange}/>
           </div>
         </Router>
       </div>
