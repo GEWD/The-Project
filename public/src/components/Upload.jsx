3d7a00f53e7156a4a6da9a609d9a24a8eedb1f76
whitzhu <whitzhu@gmail.com>
2017-03-22 20:40:40 -0700

    fix bug by converting Upload to stateful component b/c of React Router Redirect

--- public/src/components/Upload.jsx
+++ public/src/components/Upload.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class UploadReceipt extends React.Component {
+
   constructor(props) {
     super(props);
   }
 
   render() {
+
     return (
       <div>
-        <h2>Upload Your Receipt</h2>
-        <form ref='uploadForm' 
-          id='uploadForm' 
-          action='http://localhost:3000/upload' 
-          method='post' 
+        <h2>{this.props.tripName}</h2>
+        <h3>Upload Your Receipt for </h3>
+        <form ref='uploadForm'
+          id='uploadForm'
+          action='http://localhost:3000/upload'
+          method='post'
           encType="multipart/form-data">
             <input type="file" name="sampleFile" />
             <input type='submit' value='Upload!' />
-        </form>     
+        </form>
       </div>
-    );
+    )
   }
 }
-
 
 export default UploadReceipt;
