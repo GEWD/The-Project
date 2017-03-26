24c9317a93b307914d931ff4de64cc3f99163124
Eugene Song <eugenessong@gmail.com>
2017-03-26 16:57:51 -0700

    Improve receipt parsing algorithm to include accuracy check

--- server/api/vision.js
+++ server/api/vision.js
@@ -30,7 +30,7 @@ exports.promisifiedDetectText = function(image) {
 exports.spliceReceipt = function(receiptArray) {
   var itemToPriceObj = {};
   var firstPriceSearch = /\.\d\d/;
-  var totalSearch = /(\btotal)/i;
+  var totalSearch = /(\btotal|\btota)/i;
   var startSplice;
   var endSplice;
   for (var i = 0; i < receiptArray.length; i++) {
@@ -39,14 +39,29 @@ exports.spliceReceipt = function(receiptArray) {
       break;
     }
   }
-  for (var j = startSplice; j < receiptArray.length; j++) {
+  for (var j = receiptArray.length - 1; j > 0; j--) {
     if (receiptArray[j].search(totalSearch) !== -1) {
       endSplice = j;
       break;
     }
   }
   for (var k = startSplice; k < endSplice; k += 2) {
-    itemToPriceObj[receiptArray[k + 1]] = receiptArray[k];
+    if (receiptArray[k + 1].search(/sub/ig) === -1) {
+      itemToPriceObj[receiptArray[k + 1]] = receiptArray[k].replace( /[^.|\d]/g, '');
+    }
   }
+  var checkPricesAddToTotal = function() {
+    var totalPrice = Number(itemToPriceObj[Object.keys(itemToPriceObj)[Object.keys(itemToPriceObj).length - 1]]);
+    var accumPrice = 0;
+    for (var item in itemToPriceObj) {
+      accumPrice += Number(itemToPriceObj[item]);
+    }
+    //Subtract totalPrice because it will be included within the obj loop
+    accumPrice -= totalPrice;
+    if (totalPrice !== accumPrice) {
+      itemToPriceObj['<NOTE>'] = 'We apologize for the inconvenience, our <strike>sick</strike> algorithm was unable to accurately parse your receipt. Please utilize the delete and add items buttons to adjust the results and to remove this note';
+    }
+  };
+  checkPricesAddToTotal();
   return itemToPriceObj;
 };
