65b81b315fb222d021b9f8fd1e862911cd0faca9
whitzhu <whitzhu@gmail.com>
2017-03-26 16:13:00 -0700

    progress adding flex box to each item

--- public/src/components/Itemization.jsx
+++ public/src/components/Itemization.jsx
@@ -4,7 +4,6 @@ import ReceiptItemEntryList from './ReceiptItemEntryList.jsx';
 import AddMember from './AddMember.jsx';
 import {Link} from 'react-router-dom';
 
-
 const Itemization = ({addItem, itemName, itemAmount, items, selectItem, deleteItem, selectMember, addMember, member, members, onInputChange, memberExist, itemOnClick, memberOnClick, initialMemberSelect,sumTax,sumTip,calculateTotal}) => (
   <div>
     <Link to='/upload-receipt' className='back-history'>Upload Receipt</Link>
