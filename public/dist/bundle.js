6e811cd6c99f0b84ad784e41996473d97f70485e
Eugene Song <eugenessong@gmail.com>
2017-03-27 12:01:09 -0700

    Add revision to auto-divide

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -22083,7 +22083,7 @@ var App = function (_React$Component) {
         var eachPrice = itemObj.amount / itemObj.members.length;
         console.log('....??', itemObj);
         if (itemObj.members.length === 0) {
-          itemObj.members = this.state.members;
+          itemObj.members = [].concat.apply([], this.state.members);
         }
         for (var i = 0; i < itemObj.members.length; i++) {
           if (memberSum[itemObj.members[i]]) {
