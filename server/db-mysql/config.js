ed99ab8c44376185719ecebf008e8003acd81cc9
Eugene Song <eugenessong@gmail.com>
2017-03-27 12:37:48 -0700

    Revise server connection to use local DB

--- server/db-mysql/config.js
+++ server/db-mysql/config.js
@@ -1,11 +1,17 @@
-module.exports = {
-  connectionLimit: 10,
-  host: 'us-cdbr-iron-east-03.cleardb.net',
-  user: 'bc13f161df8ceb',
-  password: process.env.CLEARDB_PASS,
-  database: 'heroku_a258462d4ded143'
-};
+// module.exports = {
+//   connectionLimit: 10,
+//   host: 'us-cdbr-iron-east-03.cleardb.net',
+//   user: 'bc13f161df8ceb',
+//   password: process.env.CLEARDB_PASS,
+//   database: 'heroku_a258462d4ded143'
+// };
 
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gewd'
+};
 // SWITCH TO LOCALHOST FOR DB TESTING IF CONNETION MAX EXCEEDED
 // module.exports = {
 //   // connectionLimit: 10000,
