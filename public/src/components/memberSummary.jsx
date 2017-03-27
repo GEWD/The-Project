8758bc7014290352f2f399edcd26f90683d0041f
esong7 <eugenessong@gmail.com>
2017-03-27 00:08:09 -0700

    Remove comments

--- public/src/components/memberSummary.jsx
+++ public/src/components/memberSummary.jsx
@@ -11,29 +11,7 @@ class MemberSummary extends React.Component {
     this.sumTotal = Number(this.props.data.sumTotal);
     this.memberCount = this.props.data.members.length;
     this.perPerson = ((this.sumTax + this.sumTip) / this.memberCount);
-    // this.state = {
-    // };
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.pricePerPerson = this.pricePerPerson.bind(this);
-    // this.items = this.props.data.items;
-    // this.trip = this.data.trip;
-    // this.payor = this.data.payor;
-    // this.items_split = this.data.items_split;
-    // this.receiptName = this.data.receiptName;
-    // this.receiptUrl = this.data.receiptUrl;
-    // let itemsArray = Object.keys(data.items_split);
-
-    // this.state = {
-    // };
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.pricePerPerson = this.pricePerPerson.bind(this);
-    // this.items = this.props.data.items;
-    // this.trip = this.data.trip;
-    // this.payor = this.data.payor;
-    // this.items_split = this.data.items_split;
-    // this.receiptName = this.data.receiptName;
-    // this.receiptUrl = this.data.receiptUrl;
-    // let itemsArray = Object.keys(data.items_split);
   }
 
   
