71c6d1f3213637e1c7c6d507565bf376e0e4724f
whitzhu <whitzhu@gmail.com>
2017-03-27 14:01:53 -0700

    style summary page

--- server/db-mysql/config.js
+++ server/db-mysql/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  connectionLimit: 10,
-  host: 'us-cdbr-iron-east-03.cleardb.net',
-  user: 'bc13f161df8ceb',
-  password: process.env.CLEARDB_PASS,
-  database: 'heroku_a258462d4ded143'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gewd'
 };
