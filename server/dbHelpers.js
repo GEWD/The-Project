184a2acbb4a2fdebbaede0f68ece3ac978fd0715
Gary Wong <gary.wonghg@gmail.com>
2017-03-26 18:32:10 -0700

    Update query function for multiple receipts

--- server/dbHelpers.js
+++ server/dbHelpers.js
@@ -58,7 +58,8 @@ const queryString = {
                               (SELECT trips.id FROM trips \
                               WHERE trips.adminID = \
                               (SELECT members.id from members \
-                              WHERE members.name = ?)));',
+                              WHERE members.name = ?)\
+                              AND trips.name = ?));',
   settlePayment: '',
 
   getAllMembers: 'SELECT * FROM MEMBERS',
@@ -69,28 +70,7 @@ const queryString = {
 }
 
 const parseSummaryData = (summary) => {
-  // for (let i = 0; i < summary.items.length; i++) {
-  //   for (let k = 0; k < summary.items[i].members.length; k++) {
-  //     let itemObject = {};
-  //     let summary = {
-  //       tripName: summary.tripName,
-  //       payor: summary.username,
-  //       receiptName: summary.receiptName,
-  //       url: summary.receiptUrl,
-  //       items: [
-  //         [
-  //           itemObject.name: summary.items[i].name,
-  //           itemObject.amount: summary.items[i].amount,
-  //           itemObject.members: summary.items[i].members[k]
-  //         ]
-  //       ]
-  //     };
-  //   }
-  // }
-  // return summary;
 }
-
-
 
 const createNewUser = (userInfo) => {
   db.queryAsync(`SELECT * from members where fb_id = ?`, userInfo.fb_id)
@@ -160,7 +140,6 @@ const addMembersToTrip = (params) => {
   })
 }
 
-
 const addReceipt = (params) => {
   // Total 8 fields: get PAYOR_AUTH, TRIP_NAME, PAYOR_AUTH, RECEIPT_NAME, RECEIPT_URL, TOTAL_BILL, TOTAL_TAX, TOTAL_TAX_TIP from req.body
   console.log('addReceipt PARAMSSS!!!!', params);
@@ -186,17 +165,12 @@ const assignItemsToMembers = (allItemsArray, params) => {
 
   for (let i = 0; i < allItemsArray.length; i++) {
     for (let k = 0; k < params.items[i][0].members.length; k++) {
-      // console.log('itemNames', params.items[i][0].name);
-      // console.log('names', params.items[i][0].members[k]);
-      // let eachConsumer = params.items[i][0].members[k];
-      let count = 0;
+
       while (k !== params.items[i][0].members.length) {
         allConsumers.push(params.items[i][0].members[k]);
         allItems.push(params.items[i][0].name);
         k++;
       }
-      // return db.queryAsync(queryString.assignItemsToMembers, [params.items[i][0].name, params.receiptUrl, params.username, params.items[i][0].members[k], params.receiptUrl, params.username])
-      // .then( () => console.log('SUCCESS: assignItemsToMembers'));
     }
   }
   console.log('allitems----------------', allItems);
@@ -205,31 +179,16 @@ const assignItemsToMembers = (allItemsArray, params) => {
   // return Promise.map(allItems, (item, index) => {
   //   return Promise.map(allConsumers, (consumer, index) => {
     for (let i = 0; i < allItems.length; i++) {
-        db.query(queryString.assignItemsToMembers, [allItems[i], params.receiptUrl, params.username, allConsumers[i], params.receiptUrl, params.username])
+        db.query(queryString.assignItemsToMembers, [allItems[i], params.receiptUrl, params.username, allConsumers[i], params.receiptUrl, params.username, params.tripName])
         // return db.queryAsync(queryString.assignItemsToMembers, [allItems[i], params.receiptUrl, params.username, allConsumers[j], params.receiptUrl, params.username])
         // .then( () => console.log('SUCCESS: assignItemsToMembers'))
         // .catch( err => console.error('ERROR: insert consumed_items', err));   
     }
-      // return db.queryAsync(queryString.assignItemsToMembers, [allItems[index], params.receiptUrl, params.username, allConsumers[index], params.receiptUrl, params.username])
-      // .then( () => console.log('SUCCESS: assignItemsToMembers'))
-      // .catch( err => console.error('ERROR: insert consumed_items', err));
-  //   })
-  // })
-
-  // return Promise.map(allItemsArray, (item, index) => {
-  //   return Promise.map(memberArrayWithDupes, (member, index) => {
-
-  //     return db.queryAsync(queryString.assignItemsToMembers, [item, receiptUrl, payor, member, receiptUrl, adminName])
-  //     .then( () => console.log('SUCCESS: insert consumed_items'))
-  //     .catch( err => console.error('ERROR: insert consumed_items', err));
-  //   })
-  // })
 }
 
 const createMemberSummary = (params) => {
   console.log('----params passed down to Server here!!!!------', params);
   let tripName = params.tripName;
-
   // NEED: fb_id, name, email, token
   let adminName = params.username;
   let payor = params.username;
@@ -239,20 +198,9 @@ const createMemberSummary = (params) => {
   let sumBill = Number(params.sumBill) || 0;
   let sumTax = Number(params.sumTax) || 0;
   let sumTip = Number(params.sumTip) || 0;
-
-  // let itemSplitsArray = Object.keys(params.items_split); // ['split_1', 'split_2']
-  // let allMembersArray = [];
-  // for (let i = 0; i < itemSplitsArray.length; i++) {
-    // allMembersArray = allMembersArray.concat(params.items_split[itemSplitsArray[i]].split.payees);
-    // allMembersArray.concat(allMembersArray.concat(params.items_split[itemSplitsArray[i]].split.payees));
-  // }
-  // remote duplicates from array, just in case
-  // let noDupeMemberArray = Array.from(new Set(allMembersArray));
   let memberArrayWithDupes = [].concat.apply([], params.members);
   let noDupeMemberArray = [].concat.apply([], params.members);
   noDupeMemberArray.shift();
-  //   var arrays = [["$6"], ["$12"], ["$25"], ["$25"], ["$18"], ["$22"], ["$10"]];
-  // var merged = [].concat.apply([], arrays);
   let allItemsArray = [];
   for (let i = 0; i < params.items.length; i++) {
     allItemsArray.push(params.items[i][0].name);
@@ -287,144 +235,6 @@ const createMemberSummary = (params) => {
   .catch( err => console.error('ERROR: createMemberSummary', err));
 }
 
-  //addReceipt([payor, tripName, adminName, receiptName, receiptUrl, sumBill, sumTax, sumTip]);
-  //storeReceiptItems([receiptUrl, receiptUrl, allItemsArray, allPricesArray, 'N/A']);
-
-  // eachItemName, receiptUrl
-  // payorName
-  // each payeeName
-  // receiptUrl
-  // payorName
-
-  // for (var i = 0; i < allItemsArray.length; i++) {
-  //   for (var k = 0; k < params.items[i][0].members.length; k++)
-  //     // var eachItemName = params.items[i][0].name;
-  //     // var eachConsumer = params.items[i][0].members[k];
-
-  //     // var eachParams = [params.items[i][0].name, receiptUrl, payor, params.items[i][0].members[k], receiptUrl, payor];
-  //     db.query(queryString.assignItemsToMembers, [params.items[i][0].name, receiptUrl, payor, params.items[i][0].members[k], receiptUrl, payor], (err, result) => {
-  //     if (err) {
-  //       console.log('ERROR: assignItems in SQL', err);
-  //     }
-  //  //    else {
-  //     //  res.send(result);
-  //     // }
-  //   })
-
-  //     // assignItemsToMembers(['Candy', 'google.com/receipt01.jpg', 'Duy Nguyen', 'Gary', 'google.com/receipt01.jpg', 'Duy Nguyen']);
-  //     // assignItemsToMembers([eachItemName, receiptUrl, payor, eachConsumer, receiptUrl, payor]);
-  // }
-
-  // // let allCommentsArray = [];
-  // // for (let i = 0; i < itemSplitsArray.length; i++) {
-  // //   allCommentsArray = allCommentsArray.concat(params.items_split[itemSplitsArray[i]].split.comment);
-  // //   allCommentsArray.concat(allCommentsArray.concat(params.items_split[itemSplitsArray[i]].split.comment));
-  // // }
-
-  // // COMPLETE: add to members, trips and trips_members tables
-  //  for (let i = 0; i < noDupeMemberArray.length; i++) {
-  //    db.query(queryString.createNewUser, noDupeMemberArray[i]);
-  //  let paramsAddMembersToTrip = [tripName, adminName, noDupeMemberArray[i]];
-  //   db.query(queryString.addMembersToTrip, paramsAddMembersToTrip, (err, result) => {
-  //     if (err) {
-  //       console.log('ERROR: addMemberToTrip in SQL', err);
-  //     } else {
-  //       console.log('SUCCESS: new member has been added to the trip.')
-  //       // res.send(result);
-  //     }
-  //   })
-  // }
-  // NEED: payorID, tripID, name, url, sumBill, sumTax, sumTaxTip
-  // addReceipt([adminName, tripName, adminName, receiptName, receiptUrl, sumBill, sumTax, sumTaxTip]);
-  // // NEED: receiptURL, receiptURL, eachItemName, rawPx, comment
-  // storeReceiptItems([receiptUrl, receiptUrl, allItemsArray, allPricesArray, allCommentsArray]);
-
-  // // itemName, receiptURL, payorName, payeeName, receiptUrl, adminName
-
-  // assignItemsToMembers([allItemsArray, receiptUrl, payor, 'May', receiptUrl, adminName]);
-
-
-  // const getPayeeItemsMatrix = (params) => {
-  //   let matrix = {};
-  //   let payeeName;
-  //   let itemsList = [];
-  //   for (let i = 0; i < itemSplitsArray.length; i++) {
-  //     for (let k = 0; k < params.items_split[itemSplitsArray[i]].split.payees.length; k++)
-  //       // itemsList = params.items_split[itemSplitsArray[i]].item;
-  //       payeeName = params.items_split[itemSplitsArray[i]].split.payees[k];
-  //       if (!matrix.payeeName) {
-  //         matrix = {
-  //           payeeName:
-  //         }
-  //       }
-  //     // console.log('these are teh payees', payeeName);
-  //       matrix = {
-  //         [payeeName]: itemsList
-  //       }
-  //       // matrix.payeeName = params.items_split[itemSplitsArray[i]].item;
-  //   }
-  //   console.log('this is the matrix', matrix);
-  //   return matrix;
-  // }
-
-  // assignItemsToMembers([allItemsArray, receiptUrl, payor, allMembersArray, receiptUrl, adminName]);
-  // const queryStringInsertIntoConsumedItems = ``;
-
-  // const queryStringCheckPayorView = ``
-  // const queryStringCheckPayeeView = ``
-  // return db.queryAsync(queryString.addReceipt, params)
-  //   .then( result => {
-  //     if (!result) {
-  //       throw 'Failed in getting member summary.'
-  //     }
-  //   })
-  //   .catch ( err => console.log('ERROR in getting member summary', err));
-// }
-
-
-// const assignItemsToMembers = (params) => {
-//  // After gVision data is returned, data will be funneled and immediately be stored in 2 different tables: receipts and items table. Consumed_Items only references items table and info passed down from request headers.
-//  // There are 2 fields from items table: itemID, receiptID,
-//  // There are 3 fields from req.headers: payeeID(s), payorID (from fb_id), tripID (from trips.name);
-//   console.log(params, '-----!!!-----');
-
-//  // sequence: item_name, url, payor_auth, payee_auth, url, payor_auth
-//  // const dummyPayorAuth = ['gary@gmail.com']; // always only 1 person
-//  // const dummyReceiptURL = 'walmart.com';
-//  // const dummyPayeeAuthItems = {
-//  //  'jon@gmail.com': 'Burger',
-//  //  'may@gmail.com': 'Burger'
-//  // }
-//   // db.query(queryString.assignItemsToMembers, params, (err, result) => {
-//   //   if (err) {
-//   //     console.log('error', err);
-//   //   }
-//   // })
-//   // console.log('these are the params.....', params);
-
-//   // [ [ 'Burger', 'Fries' ],
-//   // 'google.com/receipt01.jpg',
-//   // 'Jon',
-//   // [ 'May', 'June' ],
-//   // 'google.com/receipt01.jpg',
-//   // 'Jon' ]
-//  const payeeNames = Object.keys(params[0]);
-//  for (let i = 0; i < payeeNames.length; i++) {
-//     let queryArgs = [];
-//    // let queryArgs = [dummyPayeeAuthItems[payeeNames[i]], dummyReceiptURL, dummyPayorAuth, payeeNames[i], dummyReceiptURL, dummyPayorAuth];
-//    // console.log('--------------------------', queryArgs);
-//    db.query(queryString.assignItemsToMembers, queryArgs, (err, result) => {
-//      if (err) {
-//        console.log('ERROR: assignItems in SQL', err);
-//      }
-//    //    else {
-//      //  res.send(result);
-//      // }
-//    })
-//  }
-// }
-
-
 const settlePayment = (req, res) => {
 
 }
@@ -445,7 +255,7 @@ module.exports = {
   addMembersToTrip,
   addReceipt,
   storeReceiptItems,
-  // assignItemsToMembers,
+  assignItemsToMembers,
   settlePayment,
   createMemberSummary,
   getAllUsers
