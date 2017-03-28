5e96c4a27e4ea41d6053480a88f0bb1ea5646a48
Eugene Song <eugenessong@gmail.com>
2017-03-27 21:16:48 -0700

    Revise database connection to use clearDB

--- server/db-mysql/config.js
+++ server/db-mysql/config.js
@@ -1,16 +1,16 @@
 
 
-module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'gewd'
-};
-// SWITCH TO LOCALHOST FOR DB TESTING IF CONNETION MAX EXCEEDED
 // module.exports = {
-//   // connectionLimit: 1000,
 //   host: 'localhost',
 //   user: 'root',
 //   password: '',
 //   database: 'gewd'
 // };
+// SWITCH TO LOCALHOST FOR DB TESTING IF CONNETION MAX EXCEEDED
+module.exports = {
+  connectionLimit: 1000,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gewd'
+};
