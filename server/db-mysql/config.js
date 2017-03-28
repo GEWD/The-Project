d75d2faf1502c98472b6315e407d1aa38b20dbc5
whitzhu <whitzhu@gmail.com>
2017-03-27 21:41:39 -0700

    update mysql config to point to clearDB

--- server/db-mysql/config.js
+++ server/db-mysql/config.js
@@ -1,16 +1,6 @@
-
-
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'gewd'
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'bc13f161df8ceb',
+  password: process.env.CLEARDB_PASS,
+  database: 'heroku_a258462d4ded143'
 };
-// SWITCH TO LOCALHOST FOR DB TESTING IF CONNETION MAX EXCEEDED
-// module.exports = {
-//   // connectionLimit: 1000,
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'gewd'
-// };
